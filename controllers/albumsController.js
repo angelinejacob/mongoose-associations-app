06eb54d512e875a1af4267286d986d6fae3d166a
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-10-27 00:47:09 -0400

    Album show route & create new songs completed

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -17,10 +17,31 @@ router.get('/new', (req, res) => {
     res.render('albums/new.ejs')
 })
 
+// SHOW ALBUM ROUTE
+router.get('/:albumId', (req, res) => {
+    Album.findById(req.params.albumId, (error, album) => {
+        res.render('albums/show.ejs', {
+            album
+        })
+    })
+})
+
 // CREATE NEW ALBUM
 router.post('/', (req, res) => {
     Album.create(req.body, (err, album) => {
         res.redirect('/albums')
+    })
+})
+
+// ADD A SONG
+router.post('/:albumId/songs', (req, res) =>{
+    Album.findById(req.params.albumId, (error, album) => {
+        if(error) res.send(error)
+        const newSong = new Song({title: req.body.title})
+        album.songs.push(newSong)
+        album.save((error, album) => {
+            res.redirect(`/albums/${album.id}`)
+        })
     })
 })
 
